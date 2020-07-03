3529ee17f9a848238b85d7f217dc2b184dde3c10
Torbjorn Sorby <torbjorn.sorby@zivid.com>
2020-07-03 11:15:24 +0200

    Update color_balance samples

--- source/applications/advanced/color_balance.py
+++ source/applications/advanced/color_balance.py
@@ -4,9 +4,23 @@ Balance color for RGB image.
 
 import datetime
 from dataclasses import dataclass
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import zivid
+
+
+@dataclass(frozen=True)
+class StopIncrement:
+    """
+    Set of aperture stop increments. For readability
+
+    """
+
+    full = 1 / 1
+    half = 1 / 2
+    third = 1 / 3
+    quarter = 1 / 4
 
 
 @dataclass
@@ -24,6 +38,11 @@ class MeanColor:
     red: np.array
     green: np.array
     blue: np.array
+
+
+def _get_next_aperture_setting(current_aperture: float, stop: float) -> float:
+    # new_aperture = current_aperture * 2 ^ (0.5 * stop)
+    return current_aperture * math.pow(2, 0.5 * stop)
 
 
 def _convert_frame2d_to_numpy_float_rgb(frame: zivid.Frame2D):
@@ -47,15 +66,15 @@ def _display_rgb(rgb, title):
     plt.show(block=False)
 
 
-def _compute_mean_rgb(rgb, pixels):
-    """Compute mean RGB values.
+def _get_center_mask(rgb, pixels):
+    """Get central square RGB values.
 
     Args:
         rgb: RGB image
         pixels: Number of central pixels (^2) for computation
 
     Returns:
-        mean_color: RGB channel mean values
+        Masked RGB
 
     """
     height = np.shape(rgb)[0]
@@ -64,26 +83,60 @@ def _compute_mean_rgb(rgb, pixels):
     pixel_hight_end_index = int((height + pixels) / 2)
     pixel_width_start_index = int((width - pixels) / 2)
     pixel_width_end_index = int((width + pixels) / 2)
-    red = rgb[
+    return rgb[
         pixel_hight_start_index:pixel_hight_end_index,
         pixel_width_start_index:pixel_width_end_index,
-        0,
+        :,
     ]
-    green = rgb[
-        pixel_hight_start_index:pixel_hight_end_index,
-        pixel_width_start_index:pixel_width_end_index,
-        1,
-    ]
-    blue = rgb[
-        pixel_hight_start_index:pixel_hight_end_index,
-        pixel_width_start_index:pixel_width_end_index,
-        2,
-    ]
-    mean_red = np.mean(np.reshape(red, -1), dtype=np.float64)
-    mean_green = np.mean(np.reshape(green, -1), dtype=np.float64)
-    mean_blue = np.mean(np.reshape(blue, -1), dtype=np.float64)
 
-    return MeanColor(red=mean_red, green=mean_green, blue=mean_blue)
+
+def _compute_mean_rgb(rgb):
+    """Compute mean RGB values.
+
+    Args:
+        rgb: RGB image
+
+    Returns:
+        mean_color: RGB channel mean values
+
+    """
+    mean_rgb = np.mean(np.mean(rgb, axis=0), axis=0)
+    return MeanColor(red=mean_rgb[0], green=mean_rgb[1], blue=mean_rgb[2])
+
+
+def _get_tuned_settings(camera: zivid.Camera) -> zivid.Settings2D:
+    """Get settings such that max mean(RGB) is < 240. In other words, no saturation on average.
+
+    Args:
+        camera: Zivid camera
+
+    Returns:
+        settings_2d: 2D capture settings without saturation on average
+
+    """
+    settings_2d = zivid.Settings2D(
+        acquisitions=[
+            zivid.Settings2D.Acquisition(
+                aperture=5.66,
+                exposure_time=datetime.timedelta(microseconds=80000),
+                brightness=0.0,
+                gain=2.0,
+            )
+        ],
+    )
+    rgb = _convert_frame2d_to_numpy_float_rgb(camera.capture(settings_2d))
+    rgb_masked = _get_center_mask(rgb, 100)
+    mean_rgb = np.mean(np.mean(rgb_masked, axis=0), axis=0)
+    print(f"RGB mean: {mean_rgb}, Aperture: {settings_2d.acquisitions[0].aperture}")
+    while mean_rgb.max() > 240:
+        settings_2d.acquisitions[0].aperture = _get_next_aperture_setting(
+            settings_2d.acquisitions[0].aperture, StopIncrement.quarter
+        )
+        rgb = _convert_frame2d_to_numpy_float_rgb(camera.capture(settings_2d))
+        rgb_masked = _get_center_mask(rgb, 100)
+        mean_rgb = np.mean(np.mean(rgb_masked, axis=0), axis=0)
+        print(f"RGB mean: {mean_rgb}, Aperture: {settings_2d.acquisitions[0].aperture}")
+    return settings_2d
 
 
 def _color_balance_calibration(camera, settings_2d):
@@ -110,7 +163,7 @@ def _color_balance_calibration(camera, settings_2d):
         if first_iteration:
             _display_rgb(rgb, "RGB image before color balance")
             first_iteration = False
-        mean_color = _compute_mean_rgb(rgb, 100)
+        mean_color = _compute_mean_rgb(_get_center_mask(rgb, 100))
         print(
             (
                 "Mean color values: R = "
@@ -144,26 +197,16 @@ def _main():
 
     camera = app.connect_camera()
 
-    # TODO: Automatically tune aperture to get max RGB < 220
-    settings_2d = zivid.Settings2D(
-        acquisitions=[
-            zivid.Settings2D.Acquisition(
-                aperture=5.66,
-                exposure_time=datetime.timedelta(microseconds=80000),
-                brightness=0.0,
-                gain=2.0,
-            )
-        ],
-    )
+    settings_2d = _get_tuned_settings(camera)
 
     rgb = _convert_frame2d_to_numpy_float_rgb(camera.capture(settings_2d))
     _display_rgb(rgb, "RGB image before color balance")
 
     [red_balance, blue_balance] = _color_balance_calibration(camera, settings_2d)
 
-    print("Applying color balance on 2D image")
     settings_2d.processing.color.balance.red = red_balance
     settings_2d.processing.color.balance.blue = blue_balance
+    print(f"Updating color balance settings to {settings_2d.processing.color.balance}")
     rgb_balanced = _convert_frame2d_to_numpy_float_rgb(camera.capture(settings_2d))
 
     _display_rgb(rgb_balanced, "RGB image after color balance")
